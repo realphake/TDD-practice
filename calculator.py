9869b9183ddb0c4a722aa538379ba4662dff3a83
Arjen Swellengrebel <arjen_swellengrebel@msn.com>
2015-05-02 22:29:47 +0200

    calculator adds up

--- calculator.py
+++ calculator.py
@@ -1,11 +1,31 @@
 import unittest
 
+class StringCalculator():
+    
+    def __init__(self):
+        pass
+
+    def calculate(self, calculation):
+        if "+" in calculation:
+            parts = calculation.split("+")
+            return sum( [int(i) for i in parts] )
+        return int(calculation)
+
 class testCalculator(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.calc = StringCalculator()
 
-    def testNothing(self):
-        pass
+    def testRepeatZero(self):
+        self.assertEqual(self.calc.calculate("0"), 0)
+
+    def testRepeatTwelve(self):
+        self.assertEqual(self.calc.calculate("12"), 12)
+
+    def testOnePlusTwo(self):
+        self.assertEqual(self.calc.calculate("1+2"),3)
+
+    def testSumOfThreeNumbers(self):
+        self.assertEqual(self.calc.calculate("12+1+2"),15)
 
 unittest.main()
