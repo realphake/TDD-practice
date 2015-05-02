e4e5324c2ee36c6c3845b68e368cc98b43012b82
Arjen Swellengrebel <arjen_swellengrebel@msn.com>
2015-05-02 15:03:35 +0200

    Can create game

--- tests.py
+++ tests.py
@@ -1,8 +1,18 @@
 import unittest
 
+class BowlingGame():
+
+    pass
+
 class testCalculator(unittest.TestCase) :
+    
     def testNothing(self):
         self.assertEqual(1,1)
         self.assertTrue(True)
+        
+    def testCreateGame(self):
+        game = BowlingGame()
 
 unittest.main()
+
+
