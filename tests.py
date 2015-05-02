fa825bf07ad6bc439b88687c7755e9a25983ab77
Arjen Swellengrebel <arjen_swellengrebel@msn.com>
2015-05-02 15:42:48 +0200

    Test score for ones and zeros

--- tests.py
+++ tests.py
@@ -2,20 +2,31 @@ import unittest
 
 class BowlingGame():
 
+    def __init__(self):
+        self.score = 0
+
     def roll(self, pins):
-        pass
+        self.score += pins
+
+    def calculateScore(self):
+        return self.score
 
 class testCalculator(unittest.TestCase) :
 
+    def rollBalls(self, balls, pinsPerBall):
+        for x in range(0,balls):
+            self.game.roll(pinsPerBall)
+
     def setUp(self):
         self.game = BowlingGame()
-        
-    def testNothing(self):
-        self.assertEqual(1,1)
-        self.assertTrue(True)
 
-    def testCanRoll(self):
-        self.game.roll(1)
+    def testGutterGame(self):
+        self.rollBalls(20, 0)
+        self.assertEqual(self.game.calculateScore(),0)
+        
+    def testAllOnes(self):
+        self.rollBalls(20, 1)
+        self.assertEqual(self.game.calculateScore(),20)
 
 unittest.main()
 
