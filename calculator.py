702f3d42b781812ed370ef5f73a17ca1f9a405f3
Arjen Swellengrebel <arjen_swellengrebel@msn.com>
2015-05-02 22:36:54 +0200

    multiplication

--- calculator.py
+++ calculator.py
@@ -5,10 +5,19 @@ class StringCalculator():
     def __init__(self):
         pass
 
+    def product(self,numbersToMultiply):
+        product = 1
+        for number in numbersToMultiply:
+            product *= number
+        return product
+
     def calculate(self, calculation):
         if "+" in calculation:
-            parts = calculation.split("+")
-            return sum( [int(i) for i in parts] )
+            numbersToAdd = calculation.split("+")
+            return sum( [int(i) for i in numbersToAdd] )
+        if "*" in calculation:
+            numbersToMultiply = calculation.split("*")
+            return self.product([int(i) for i in numbersToMultiply])
         return int(calculation)
 
 class testCalculator(unittest.TestCase):
@@ -16,16 +25,13 @@ class testCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = StringCalculator()
 
-    def testRepeatZero(self):
-        self.assertEqual(self.calc.calculate("0"), 0)
-
     def testRepeatTwelve(self):
         self.assertEqual(self.calc.calculate("12"), 12)
 
-    def testOnePlusTwo(self):
-        self.assertEqual(self.calc.calculate("1+2"),3)
-
-    def testSumOfThreeNumbers(self):
+    def testAddition(self):
         self.assertEqual(self.calc.calculate("12+1+2"),15)
+
+    def testMultiplication(self):
+        self.assertEqual(self.calc.calculate("3*2*4"),24)
 
 unittest.main()
