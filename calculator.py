668a9fde567bac07b7e4e725f086d86566620afd
Arjen Swellengrebel <arjen_swellengrebel@msn.com>
2015-05-02 22:13:01 +0200

    New testing practice

---
+++ calculator.py
@@ -0,0 +1,11 @@
+import unittest
+
+class testCalculator(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def testNothing(self):
+        pass
+
+unittest.main()
---
+++ empty tests.py
@@ -0,0 +1,12 @@
+import unittest
+
+class testSuite(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def testNothing(self):
+        pass
+
+unittest.main()
+
