a85607427bf6aa77ed800e1b47ce123234bf20f1
Arjen Swellengrebel <arjen_swellengrebel@msn.com>
2015-05-02 17:02:37 +0200

    Strike case works

--- tests.py
+++ tests.py
@@ -11,15 +11,27 @@ class BowlingGame():
     def scoreInFrame(self, frameStart):
         return self.allRolls[frameStart] + self.allRolls[frameStart+1]
 
+    def isStrike(self, frameStart):
+        return self.allRolls[frameStart] >= 10
+
     def isSpare(self, frameStart):
         return self.scoreInFrame(frameStart) >= 10
 
     def calculateScore(self):
         score = 0
-        for frameStart in range(0,len(self.allRolls),2):
-            score += self.scoreInFrame(frameStart)
+        frameStart = 0
+        while frameStart < len(self.allRolls):
+            if self.isStrike(frameStart):
+                score += self.allRolls[frameStart]
+                score += self.scoreInFrame(frameStart+1)
+                frameStart += 1
             if self.isSpare(frameStart):
+                score += self.scoreInFrame(frameStart)
                 score += self.allRolls[frameStart+2]
+                frameStart += 2
+            else:
+                score += self.scoreInFrame(frameStart)
+                frameStart += 2
         return score
 
 class testCalculator(unittest.TestCase) :
@@ -44,6 +56,12 @@ class testCalculator(unittest.TestCase) :
         self.rollBalls(1,7)
         self.rollBalls(17,0)
         self.assertEqual(self.game.calculateScore(),24)
+
+    def testOneStrike(self):
+        self.rollBalls(1,10)
+        self.rollBalls(2,3)
+        self.rollBalls(16,0)
+        self.assertEqual(self.game.calculateScore(),22)
 
 unittest.main()
 
